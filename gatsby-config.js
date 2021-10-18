22f4ab4f57503b7b3cc7d7b5ff892753457ee3d1
Melissa <36255876+jmpstart@users.noreply.github.com>
2021-10-17 22:08:19 -0700

    contentful
    Contentful auth finally working.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,44 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+const { CONTENTFUL_SPACE_ID, CONTENTFUL_DELIVERY_TOKEN, CONTENTFUL_ACCESS_TOKEN } = process.env
+
+if (!CONTENTFUL_SPACE_ID || !CONTENTFUL_ACCESS_TOKEN) {
+  throw new Error(
+    "Contentful spaceId and the access token need to be provided."
+  )
+}
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby 9689ff demo`,
+    locale: "en",
+    title: "Tigerbite",
+    description: "Data-driven advertising management.",
+    author: "Melissa Whitman",
+    
   },
   plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-netlify",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/posts`,
-        name: `blog`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [],
       },
     },
-    `gatsby-plugin-netlify`,
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: 
+          process.env.CONTENTFUL_DELIVERY_TOKEN ||
+          process.env.CONTENTFUL_ACCESS_TOKEN,
+        downloadLocal: true,
+      },
+    },
   ],
-}
+};
